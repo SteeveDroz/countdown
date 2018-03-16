8190b3f31aa4514ce0ff2633ce0eecf28912ac1a
Steeve Droz <steeve.droz@sudest-webdesign.com>
2018-03-16 08:47:33 +0100

    Initialize data

--- countdown.js
+++ countdown.js
@@ -11,7 +11,17 @@ const load = () => {
     }, 1000 / 60)
 }
 
-const init = data => {}
+const init = data => {
+    data.canvas = document.querySelector('#countdown')
+    data.ctx = data.canvas.getContext('2d')
+
+    data.time.hours = 0
+    data.time.minutes = 0
+    data.time.seconds = 0
+
+    data.canvas.width = data.canvas.height = 300
+}
+
 const update = data => {}
 const render = data => {}
 
